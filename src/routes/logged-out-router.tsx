46c6e9a906be41e1e3a382d9691a2dd37f21fd67
daehyeong2 <baconbacon1231@gmail.com>
2024-09-09 15:57:41 +0900

    React Hook Form part Two (17.2)

--- src/routes/logged-out-router.tsx
+++ src/routes/logged-out-router.tsx
@@ -1,19 +1,23 @@
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export const LoggedOutRouter = () => {
   const {
     register,
     watch,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IForm>();
   const onSubmit = () => {
     console.log(watch());
   };
   const onInvalid = () => {
     console.log("Cannot create account");
   };
-  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
@@ -22,14 +26,23 @@ export const LoggedOutRouter = () => {
           <div>
             <input
               {...register("email", {
-                required: true,
+                required: "이메일은 필수 항목입니다.",
                 validate: (email: string) => email.includes("@gmail.com"),
               })}
               name="email"
               placeholder="email"
               type="email"
-              required
             />
+            {errors.email?.message && (
+              <span className="font-semibold text-red-600">
+                {errors.email.message}
+              </span>
+            )}
+            {errors.email?.type === "validate" && (
+              <span className="font-semibold text-red-600">
+                @gmail.com 만 사용 가능합니다.
+              </span>
+            )}
           </div>
           <div>
             <input
