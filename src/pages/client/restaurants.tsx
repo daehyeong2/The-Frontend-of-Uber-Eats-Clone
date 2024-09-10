dbfd20deec0dcd0711004f4b45b99db8036d6aa0
daehyeong2 <baconbacon1231@gmail.com>
2024-09-11 07:22:39 +0900

    Categories Style (19.1)

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -47,8 +47,43 @@ const Restaurants = () => {
       },
     },
   });
-  console.log(data);
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-32 flex justify-center items-center relative overflow-hidden">
+        <img
+          src="/images/vegetables.png"
+          className="object-cover object-center absolute w-full brightness-[60%]"
+        />
+        <input
+          type="search"
+          placeholder="Search restaurants.."
+          className="input rounded-lg border-none w-1/4 z-10"
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mt-5">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category, idx) => (
+              <div
+                className="flex flex-col gap-1 items-center cursor-pointer"
+                key={idx}
+              >
+                <div
+                  className="rounded-full size-14 flex flex-col justify-center items-center bg-contain hover:bg-gray-100"
+                  style={{
+                    backgroundImage: `url("${category.icon}")`,
+                  }}
+                />
+                <span className="text-sm font-semibold font-freesentation">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Restaurants;
