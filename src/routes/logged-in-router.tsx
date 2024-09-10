b05fc67222e85190aaf903cf016fd6bf26a50cca
daehyeong2 <baconbacon1231@gmail.com>
2024-09-10 13:09:07 +0900

    Using the Token (17.15)

--- src/routes/logged-in-router.tsx
+++ src/routes/logged-in-router.tsx
@@ -1,17 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
 import { isLoggedInVar } from "../apollo";
 import { LOCALSTORAGE_TOKEN } from "../constants";
+import nuberLogo from "../logo.svg";
+import { meQuery } from "../__generated__/meQuery";
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
 
 export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  console.log(data, error);
   const onClick = () => {
     localStorage.removeItem(LOCALSTORAGE_TOKEN);
     isLoggedInVar(false);
   };
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex flex-col gap-10 justify-center items-center">
+        <img src={nuberLogo} className="w-52" alt="logo" />
+        <ul className="flex gap-5">
+          <li className="h-3 w-3 bg-black rounded-full animate-bounce-big" />
+          <li className="h-3 w-3 bg-black rounded-full animate-bounce-big animation-delay-100" />
+          <li className="h-3 w-3 bg-black rounded-full animate-bounce-big animation-delay-200" />
+        </ul>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Logged In</h1>
-      <button onClick={onClick} className="btn">
-        Log Out
-      </button>
+      <h1>{data.me.email}</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,20 @@ module.exports = {
         neutral: colors.neutral,
         lime: colors.lime,
       },
+      animation: {
+        "bounce-big": "bounce-big 750ms ease-in-out infinite",
+      },
+      keyframes: {
+        "bounce-big": {
+          "0%": { transform: "none" },
+          "50%": { transform: "translateY(-10px)" },
+          "100%": { transform: "none" },
+        },
+      },
     },
     fontFamily: {
       freesentation: ["freesentation"],
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animation-delay")],
 };
