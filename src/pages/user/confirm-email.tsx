ec43eb8be8cbae6be217aa753859807c0b3d11b7
daehyeong2 <baconbacon1231@gmail.com>
2024-09-11 03:50:46 +0900

    Verifying Email part Two (18.1)

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,4 +1,4 @@
-import { gql, useMutation } from "@apollo/client";
+import { gql, useApolloClient, useMutation } from "@apollo/client";
 import {
   verifyEmail,
   verifyEmailVariables,
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useEffect } from "react";
 import useQueryParams from "../../hooks/useQueryParams";
+import useMe from "../../hooks/useMe";
 
 const VERIFY_EMAIL_MUTATION = gql`
   mutation verifyEmail($code: String!) {
@@ -18,10 +19,32 @@ const VERIFY_EMAIL_MUTATION = gql`
 `;
 
 const ConfirmEmail = () => {
-  const [verifyEmail, { loading: verifyingEmail }] = useMutation<
-    verifyEmail,
-    verifyEmailVariables
-  >(VERIFY_EMAIL_MUTATION);
+  const { data: userData } = useMe();
+  const client = useApolloClient();
+  const onCompleted = (data: verifyEmail) => {
+    const {
+      verifyEmail: { ok },
+    } = data;
+    if (ok && userData?.me.id) {
+      client.writeFragment({
+        id: `User:${userData.me.id}`,
+        fragment: gql`
+          fragment VerifiedUser on User {
+            verified
+          }
+        `,
+        data: {
+          verified: true,
+        },
+      });
+    }
+  };
+  const [verifyEmail] = useMutation<verifyEmail, verifyEmailVariables>(
+    VERIFY_EMAIL_MUTATION,
+    {
+      onCompleted,
+    }
+  );
   const param = useQueryParams();
   useEffect(() => {
     const code = param.get("code");
@@ -31,7 +54,7 @@ const ConfirmEmail = () => {
         code,
       },
     });
-  }, []);
+  }, [param, verifyEmail]);
   return (
     <div className="absolute top-0 h-screen w-screen flex flex-col gap-4 justify-center items-center -z-10">
       <FontAwesomeIcon
