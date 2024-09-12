c71c553aed61ed835b9f46af5669ff27e2f5dc44
daehyeong2 <baconbacon1231@gmail.com>
2024-09-13 01:08:50 +0900

    App Tests (20.1)

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,5 @@
-import { LoggedOutRouter } from "../routes/logged-out-router";
-import { LoggedInRouter } from "../routes/logged-in-router";
+import { LoggedOutRouter } from "../routers/logged-out-router";
+import { LoggedInRouter } from "../routers/logged-in-router";
 import { useReactiveVar } from "@apollo/client";
 import { isLoggedInVar } from "../apollo";
 import "../styles/tailwind.css";
