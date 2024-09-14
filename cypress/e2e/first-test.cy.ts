4734741698185b4dd342465274f748bac84d9617
daehyeong2 <baconbacon1231@gmail.com>
2024-09-14 12:49:18 +0900

    Our First Cypress Test (21.1)

--- cypress/e2e/first-test.cy.ts
+++ cypress/e2e/first-test.cy.ts
@@ -1,7 +1,21 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Nuber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("test@test.com")
+      .findByPlaceholderText(/password/i)
+      .type("test1234")
+      .findByRole("button")
+      .should("not.be.disabled");
+  });
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+      .findByRole("alert")
+      .should("have.text", "올바른 이메일을 입력해 주세요.");
   });
 });
